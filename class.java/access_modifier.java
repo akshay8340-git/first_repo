4b9d788f17e25c8e3944358ca6cccc7aa257abc7
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2025-02-25 16:10:33 +0530

    first commit

--- class.java/access_modifier.java
+++ class.java/access_modifier.java
@@ -1,40 +1,36 @@
 
 
 class Circle{
-
     double radius;
     double area;
 
-    public void setRadius(double radius){
-        if(radius > 0){
-           this.radius = radius;
-           calculateArea();
+    public void setRadius(double r){
+        if(r > 0){
+            this.radius = r;
         }
         else{
-            System.out.println("radius must be postive");
+            System.out.println("the radius must be positive");
         }
     }
-
     public double getRadius(){
         return radius;
     }
 
-    public void calculateArea(){
-        this.area = 3.14f * radius * radius;
+    public double calculateArea(){
+       return Math.PI * radius * radius;
     }
-
-    public double getArea(){
-        return area;
-    }
-
 }
 
 public class access_modifier {
     public static void main(String[] args) {
 
-        Circle c = new Circle();
-        c.setRadius(5.0);
-        System.out.println(c.getArea());
+
+        Circle cc = new Circle();
+
+        cc.setRadius(10);
+
+        System.out.println(cc.getRadius());
+        System.out.println(cc.calculateArea());
 
     }
 }
